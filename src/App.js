9182574e53dc6b22cca92510f16a754fd600d7c0
sakibhusainzaidi2000@gmail.com <sakibhusainzaidi2000@gmail.com>
2024-02-20 11:04:12 +0530

    example of useState and setState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+ // state current value ko rkhta hai ab hmne hooks use nhi kiya hai 
+  // is bjah se count ki value add nhi ho rhi hai 
+  // let count = 0;
+  // function handleADD(){
+  //   count += 1; 
+  //   console.log(count); 
+  // }
+
+  const [count, setCount] = useState(0);
+  function handleADD(){
+    setCount(count + 1);
+  }
+  function handleSUB(){
+    setCount(count - 1);
+  }
+  function handleReset(){
+    setCount(0);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="APP">
+      <div className="box">
+        <p>{count} </p>
+        {/* if we pass handleADD() so it will execute run time  */}
+        <button onClick={handleADD} classname = "add">ADD</button> 
+               {/* onClick = {()=>setCount(count+1)} */}
+        <button onClick={handleSUB} className="sub">SUB</button>
+        <button onClick={handleReset} className='reset'></button>
+      </div>
     </div>
   );
 }
